c2b0f0d758b51cba6ba398e95f832ac710ece249
Nata Inditama <natainditama@gmail.com>
2023-03-18 17:56:22 +0800

    style(): Implement design page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './styles/style.css'
-import jQuery from "jQuery";
+import gsap  from "gsap"
 
-jQuery("#greetings span").each(function() {
-  console.log(jQuery(this));
+const title = document.querySelector("#greetings");
+const letters = title?.querySelectorAll("span");
+let tl = gsap.timeline({ repeat: -1, });
+
+letters?.forEach(function(letter: HTMLSpanElement, index: number) {
+    tl.to(letter, 1, { className: "glow" }).to(letter, 1, {
+      className: "",
+      repeat: -1,
+      yoyo: true,
+    });
 })
+
+
